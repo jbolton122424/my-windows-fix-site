f68a2334515ff2688bf8de8d90815f45b5acd1ef
Jason Bolton <jrbolt@aero-jrbolt-mbp.lan>
2026-01-06 16:14:40 -0500

    Fix robots.txt route

--- app/robots.txt/route.js
+++ app/robots.txt/route.js
@@ -1,34 +1,13 @@
+// app/robots.txt/route.js
 export async function GET() {
-Sitemap: https://my-windows-fix-site-1.vercel.app/sitemap.xml
+  const content = `User-agent: *
+Disallow:
 
-  const pages = [
-    "",
-    "/fix/0x80070422",
-    "/fix/0x80070005",
-    "/fix/0x80072ee7",
-    "/fix/0x80070424",
-    "/fix/0x80070570",
-    "/fix/0x80070057",
-    "/fix/0x80072f8f",
-    "/fix/0x80070020"
-  ];
+Sitemap: https://my-windows-fix-site-1.vercel.app/sitemap.xml`;
 
-  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages
-  .map(
-    (path) => `
-  <url>
-    <loc>${baseUrl}${path}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-  </url>`
-  )
-  .join("")}
-</urlset>`;
-
-  return new Response(sitemapXml, {
+  return new Response(content, {
     headers: {
-      "Content-Type": "application/xml"
-    }
+      "Content-Type": "text/plain",
+    },
   });
 }
